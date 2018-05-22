0b74610a253ccc1ddbf855688c837743c49253fc
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-05-21 22:41:54 -0400

    Conditional Statements HackerRank

---
+++ HackerRank_ConditionalStatements.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if((N % 2 == 1) || ((N % 2 == 0) && (N >=6 && N <= 20)))
+        {
+        cout << "Weird" << endl;
+        }
+    else
+        {
+        cout << "Not Weird" << endl;
+        }
+
+    return 0;
+}
