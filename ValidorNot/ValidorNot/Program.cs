d5359ba85f734bbce6caef9fd76284dd6dfa8ab3
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2018-05-27 10:10:08 -0400

    ValidOrNot

---
+++ ValidorNot/ValidorNot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ValidorNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Please enter a number between 1 and 10: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Not Valid");
+        
+        }
+    }
+}
